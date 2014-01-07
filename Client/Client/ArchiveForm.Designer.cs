a4e210dccc7ca8f9f3ae104e3658ca4aa9dd6ee3
Bartosz Kaaz <bartek47@gmail.com>
2014-01-07 01:57:49 +0100

    html in archive

--- Client/Client/ArchiveForm.Designer.cs
+++ Client/Client/ArchiveForm.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ArchiveContainer = new System.Windows.Forms.RichTextBox();
+            this.textMessageContainer = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // ArchiveContainer
+            // textMessageContainer
             // 
-            this.ArchiveContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.textMessageContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ArchiveContainer.Location = new System.Drawing.Point(12, 12);
-            this.ArchiveContainer.Name = "ArchiveContainer";
-            this.ArchiveContainer.ReadOnly = true;
-            this.ArchiveContainer.Size = new System.Drawing.Size(459, 466);
-            this.ArchiveContainer.TabIndex = 6;
-            this.ArchiveContainer.Text = "";
+            this.textMessageContainer.Location = new System.Drawing.Point(12, 12);
+            this.textMessageContainer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.textMessageContainer.Name = "textMessageContainer";
+            this.textMessageContainer.ScriptErrorsSuppressed = true;
+            this.textMessageContainer.Size = new System.Drawing.Size(459, 466);
+            this.textMessageContainer.TabIndex = 6;
             // 
             // ArchiveForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(483, 490);
-            this.Controls.Add(this.ArchiveContainer);
+            this.Controls.Add(this.textMessageContainer);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ArchiveForm";
@@ -59,6 +59,7 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox ArchiveContainer;
+        private System.Windows.Forms.WebBrowser textMessageContainer;
+
     }
 }
--- Client/Client/ArchiveForm.cs
+++ Client/Client/ArchiveForm.cs
@@ -8,15 +8,21 @@ namespace Komin
         public ArchiveForm(string archivePath)
         {
             InitializeComponent();
-
+            textMessageContainer.DocumentText = "";
             try
             {
                 var archiveContent = System.IO.File.ReadAllText(archivePath);
-                ArchiveContainer.Text += archiveContent;
+                textMessageContainer.Document.Write(archiveContent);
+
             }
             catch (Exception)
             {
-                ArchiveContainer.Text += "Brak danych w archiwum.";
+                textMessageContainer.Document.Write("<div style='width:100%; height:100%; " +
+                                                    "font-family: Verdana, Tahoma, Arial; " +
+                                                    "background-color: #EEEEEE; " +
+                                                    "text-align: center; padding: 100px'>" +
+                                                    "Brak danych w archiwum." +
+                                                    "</div>");
             }
 
         }
